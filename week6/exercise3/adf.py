39632776eb8ac387449d41e1bafeab2753447e11
sunjunhqq <sunjunhqq@gmail.com>
2022-09-23 11:44:05 +0800

    Update week6/exercise3

--- week6/exercise3/adf.py
+++ week6/exercise3/adf.py
@@ -1,3 +1,5 @@
+from math import fabs
+from operator import truediv
 import torch
 
 from torch import nn
@@ -10,10 +12,8 @@ from torchvision.transforms import ToTensor
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
-
 import numpy as np
 import ast
-
 
 class CensusNet(nn.Module):
     def __init__(self):
@@ -48,6 +48,25 @@ def load_model(model_class, name):
 
     return model
 
+def isDiscriminative(x, model):
+    print('Sample x is: {}'.format(x[0].tolist()))
+    x.requires_grad = True
+    pred_x = model(x)
+    #print('Prediction of x is: {}'.format(pred_x[0].tolist()))
+    #print('Label of x is: {}'.format(torch.argmax(pred_x).item()))
+
+    xp = x.detach().clone()
+    xp[0][8] = 1-xp[0][8]
+    print('Sample xp is: {}'.format(xp[0].tolist()))
+    pred_xp = model(xp)
+    #print('Prediction of xp is: {}'.format(pred_xp[0].tolist()))
+    #print('Label of xp is: {}'.format(torch.argmax(pred_xp).item()))
+    if torch.argmax(pred_x).item() != torch.argmax(pred_xp).item():
+        print('This sample is discriminary.')
+        return True
+    else:
+        print('This sample is NOT discriminary.')
+        return False 
 
 device = 'cpu'
 
@@ -62,28 +81,29 @@ for i in range(1):
     x = x.reshape(1, 13)
     x = torch.Tensor(x)
 
+    #The following captures how the global search works
+    while isDiscriminative(x,model)==False:
+        x.requires_grad = True
+        pred_x = model(x)
+        loss = F.cross_entropy(pred_x, labels)
+        loss.backward()
+        print('Gradient of x: {}'.format(x.grad.data.sign()[0].tolist()))
+        x = x.detach().clone()
+        x = x + x.grad.data.sign()
+
+    print("Discriminative instance found.")
+    
+    #The following examplifies how local search works
     print('Sample x is: {}'.format(x[0].tolist()))
     x.requires_grad = True
-
     pred_x = model(x)
-    print('Prediction of x is: {}'.format(pred_x[0].tolist()))
-    print('Label of x is: {}'.format(torch.argmax(pred_x).item()))
     loss = F.cross_entropy(pred_x, labels)
     loss.backward()
+    grad = x.grad.data
+    print('Gradient of x: {}'.format(grad.tolist()))    
+    x = x.detach().clone()
+    #TODO: Based on the above-printed grad, update x according to the idea of the local search to find one discirminary instance. 
+    #TODO: Your code goes here. Only one/two lines are needed.
 
-    print('Gradient of x: {}'.format(x.grad.data.sign()[0].tolist()))
 
-    x1 = x.detach().clone()
-    x1 = x1 + x.grad.data.sign()
-    print('Updated sample x1 is: {}'.format(x1[0].tolist()))
-
-    x1.requires_grad = True
-    pred_x1 = model(x1)
-    print('Predication of x1 is {}'.format(pred_x1[0].tolist()))
-    print('Label of x1 is {}'.format(torch.argmax(pred_x1).item()))
-
-    loss1 = F.cross_entropy(pred_x1, labels)
-    loss1.backward()
-
-    print('Gradient of x1: {}'.format(x1.grad.data))
-
+    isDiscriminative(x, model)
